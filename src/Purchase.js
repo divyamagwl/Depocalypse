d8ae509fccbaa6494a25315bea90d60d77cec124
Nikhil Agarwal <nikhil.agarwal@iiitb.org>
2021-07-24 14:41:20 +0530

    added purchase screen

---
+++ src/Purchase.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Purchase.css'
+
+function Purchase() {
+    return (
+        <div className='purchase'>
+            <div className="purchase__artwork">
+                <h1>artwork here</h1>
+            </div>
+
+            <div className="purchase__details">
+                <h1>Hello, I am details</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Purchase
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import CreateNFT from './containers/CreateNFT';
 import Market from './Market';
 import './BaseRouter.css'
+import Purchase from './Purchase';
 
 const BaseRouter = () => (
   <div className='base__router'>
     <Switch>
       <Route exact path='/' component={Market} />
       <Route exact path='/create-nft' component={CreateNFT} />
+      <Route exact path='/nft' component={Purchase} />
     </Switch>
   </div>
 );
