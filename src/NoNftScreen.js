47d2215a9bfcd666e8d11ff3a719a26f14646ea3
Nikhil Agarwal <nikhil.agarwal@iiitb.org>
2021-07-31 18:50:26 +0530

    No Nft screen created

---
+++ src/NoNftScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NoNftScreen.css';
+
+function NoNftScreen() {
+    return (
+        <div className='nonftscreen'>
+           <h1> No NFTs found 😔</h1>
+           <p>You can create your own nft using Creat NFT page 😁</p>
+        </div>
+    )
+}
+
+export default NoNftScreen
